5cbda6e9c5ff612f74875daf7c0108d4f53c4705
RishabhSTeja <rsteja@rishabh.teja>
2021-05-25 16:02:34 +0530

    Added main files to the repository

---
+++ astroplan_hct_iia.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+import socket
+import datetime
+import numpy as np
+import dict_from_gui
+import astropy.units as u
+from pytz import timezone
+import matplotlib.pyplot as plt
+from astropy.time import Time, TimeDelta
+from astroplan import FixedTarget, Observer
+from astropy.coordinates import SkyCoord, EarthLocation
+from astroplan.plots import plot_altitude, dark_style_sheet, plot_finder_image, plot_airmass
+
+
+# In[ ]:
+timezoneIAO = timezone('Asia/Kolkata')
+timezoneIAO = timezoneIAO.localize(datetime.datetime(2012, 7, 10, 12, 0))
+hct = Observer.at_site('iao',timezone= timezoneIAO.tzinfo)
+
+def is_connected():
+    try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        socket.create_connection(("1.1.1.1", 53))
+        return True
+    except OSError:
+        pass
+    return False
+
+"""
+params_dict= {'obsDate':cal.get_date(),
+'AltPlot': CheckVarAlt.get(),
+'AirMassPlot': CheckVarAirmass.get(), 
+'SaveBothPlots': SavePlots.get(),
+'ObjSkyPlot':ObjSky.get(),
+'Object': NameObject.get(), 
+'RA': RA_Object.get(),
+'Dec': Dec_Object.get() }
+"""
+params_dict = dict_from_gui.get_values()
+
+
+
+yyyy, mm, dd = params_dict['obsDate'].split(' ')
+dd_next = str(int(dd)+1) 
+
+
+obj_name = str(params_dict['Object'])
+RA_hr, RA_minutes, RA_secs = params_dict['RA'].split(':')
+dec_deg, dec_min, dec_secs = params_dict['Dec'].split(':')
+ALTPLOT = int(params_dict['AltPlot'])
+AIRMASSPLOT = int(params_dict['AirMassPlot'])
+SAVEPLOTS = int(params_dict['SaveBothPlots'])
+OBJSKYPLOT = int(params_dict['ObjSkyPlot'])
+
+
+
+#Define Object 
+RA = RA_hr+'h'+RA_minutes+'m'+RA_secs+'s'
+DEC = dec_deg+'d'+dec_min+'m'+dec_secs+'s'
+
+obj_coord = SkyCoord(ra=RA, dec=DEC)
+obj = FixedTarget(coord=obj_coord, name=obj_name)
+
+delta_t = TimeDelta(12.0*3600*u.s, scale='tai')
+
+
+
+
+#define time intervals 
+start_time = Time(yyyy+'-'+mm+'-'+dd+' 13:00:00')
+delta_t = TimeDelta(12.0*3600*u.s, scale='tai')
+observing_time = start_time + delta_t*np.linspace(0,1,100)
+
+
+
+fig, ax = plt.subplots(figsize=(8,6))
+
+ax.set_title('Time Variation')
+if ALTPLOT and AIRMASSPLOT:
+    plot_altitude(obj, hct, observing_time, brightness_shading=True,ax=ax,
+                  airmass_yaxis=True)    
+    ax.axhline(25,c='red')
+    ax.legend([obj_name],shadow=True,loc='upper right')
+    plt.show()
+    if SAVEPLOTS:
+        fig.savefig(obj_name.strip(' ')+'_AirmassAlt_plot_'+params_dict['obsDate'].replace(' ','-')+'.pdf',
+                bbox_inches='tight')
+elif AIRMASSPLOT:
+    plot_airmass(obj, hct, observing_time,brightness_shading=True, ax=ax)    
+    ax.legend([obj_name],shadow=True,loc='upper right')
+    plt.show()
+    if SAVEPLOTS:
+        fig.savefig(obj_name.strip(' ')+'_airmass_plot_'+params_dict['obsDate'].replace(' ','-')+'.pdf',
+                bbox_inches='tight')
+    
+elif ALTPLOT:
+    plot_altitude(obj, hct, observing_time,brightness_shading=True,ax=ax)    
+    ax.axhline(25,c='red')
+    ax.legend([obj_name],shadow=True,loc='upper right')
+    plt.show()
+    if SAVEPLOTS:
+        fig.savefig(obj_name.strip(' ')+'_alt_plot_'+params_dict['obsDate'].replace(' ','-')+'.pdf',
+            bbox_inches='tight')
+    
+
+if OBJSKYPLOT:
+    print("Checking Internet Connection To retrieve sky maps\n")
+    if is_connected():
+        import warnings
+        warnings.filterwarnings('ignore')
+        import matplotlib.pyplot as plt1
+        fig1, ax1 = plt1.subplots(figsize=(6,6))
+        plt1.axis('off')
+        fig1.suptitle('ra='+RA+' dec='+DEC)
+        ax1, hdu = plot_finder_image(obj,reticle=True,grid=True, fov_radius=10*u.arcmin)
+        fig1.show()
+        plt1.show()
+        
+        if SAVEPLOTS:
+            fig1.savefig(obj_name.strip(' ')+'_sky_'+params_dict['obsDate'].replace(' ','-')+'.pdf',
+                    bbox_inches='tight')
+    else :
+        print("No Internet Connection\nExiting. Have a nice night.")
+        exit()
+    
+exit()
+
---
+++ dict_from_gui.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import socket
+import numpy as np
+import tkinter as tk
+from tkinter.ttk import *
+from pytz import timezone
+from PIL import ImageTk, Image
+from tkcalendar import Calendar
+
+"""
+
+This module tkinter library to get user input from GUI window
+
+"""
+
+def get_values():
+
+    """ get_values gives the values in the form of dict :
+     params_dict= {'obsDate':str (yyyy-mm-dd),
+    'AltPlot': bool,
+    'AirMassPlot': bool, 
+    'SaveBothPlots': bool,
+    'ObjSkyPlot':bool,
+    'Object': str , 
+    'RA': str (hh:mm:ss),
+    'Dec': str (dd:mm:ss)}
+    """
+    
+    def grad_date():
+        date.config(text = "Selected Date is: " + cal.get_date())
+
+    def close_window():
+        top.destroy()
+
+    top = tk.Tk()
+    top.title('Object Visibility Plots at HCT, IAO')
+    top.resizable(True, True)
+    top.configure(bg='black')
+
+
+    CheckVarAlt = tk.IntVar(top)
+    CheckVarAirmass = tk.IntVar(top)
+    SavePlots = tk.IntVar(top)
+    ObjSky = tk.IntVar(top)
+    NameObject = tk.StringVar(top)
+    RA_Object = tk.StringVar(top)
+    Dec_Object = tk.StringVar(top)
+
+    plot_for_altitude = tk.Checkbutton(top, text='Alt. v Time Plot', selectcolor='blue',
+                                       variable= CheckVarAlt,fg='white',bg='black',activebackground='red',
+                                       onvalue=1, offvalue=0, height=6, width=20, anchor='w', font=('Havletica', 12))
+    plot_for_airmass = tk.Checkbutton(top, text='Airmass v Time Plot',selectcolor='blue',
+                                      variable=CheckVarAirmass,fg='white',bg='black',activebackground='red',
+                                      onvalue=1, offvalue=0, height=6, width=20, anchor='w', font=('Havletica', 12))
+
+    save_plots = tk.Checkbutton(top, text='Save Plots', variable=SavePlots,activebackground='red',
+                                onvalue=1,fg='white',bg='black',selectcolor='blue',
+                               offvalue=0, height=6, width=20, anchor='w', font=('Havletica', 12))
+
+    obj_finder_plot = tk.Checkbutton(top, text='Object in Sky Plot', variable=ObjSky,activebackground='red',
+                                onvalue=1,fg='white',bg='black',selectcolor='blue',
+                               offvalue=0, height=6, width=20, anchor='w', font=('Havletica', 12))
+
+    cal = Calendar(top, selectmode='day', date_pattern='yyyy mm dd',
+                   year= 2021, month=6, day=1)
+    date = tk.Label(top, text='Select Obs Day ', fg='white', bg='black',font=('DejaVu',12))
+    close_button = tk.Button(top, text = "Click and Quit", command = close_window,font=('DejaVu',12), bg='white',fg='red')
+
+
+    Object_params = tk.Label(top, text='Write Object Properties', font=("DejaVu", 14),
+                             fg='yellow', bg='black', padx=4,pady=4)
+    Plot_params = tk.Label(top, text=' Plot Options (Choose Multiple)', font=("DejaVu", 14),
+                           fg='red', bg='black', padx=4,pady=4)
+
+    Object_name = tk.Label(top, text=' Name of the Object', bg='black',fg='white',font=('Havletica', 12))
+    Object_name_entry = tk.Entry(top, bd=5, textvariable=NameObject,font=('Havletica', 12))
+
+    Object_RA = tk.Label(top, text=" Object's RA (hh:mm:ss)",bg='black', fg='white', font=('Havletica', 12))
+    Object_RA_entry = tk.Entry(top, bd=5, textvariable=RA_Object,font=('Havletica', 12))
+
+    Object_dec = tk.Label(top, text = "Object's dec (dd:mm:ss)", bg='black', fg='white', font=('Havletica', 12))
+    Object_dec_entry = tk.Entry(top, bd=5, textvariable=Dec_Object,font=('Havletica', 12))
+
+    Object_params.grid(row=0, columnspan=2, column=0)
+    Plot_params.grid(row=0, columnspan=2, column=2)
+
+
+    Object_name.grid(row=1,column=0)
+    Object_name_entry.grid(row=1,column=1)
+    Object_RA.grid(row=2,column=0)
+    Object_RA_entry.grid(row=2,column=1)
+    Object_dec.grid(row=3,column=0)
+    Object_dec_entry.grid(row=3,column=1)
+
+    date.grid(row=4, columnspan=2)
+    cal.grid(row=5, columnspan=2, rowspan=2, column=0)
+    plot_for_altitude.grid(row=1, column=3)
+    plot_for_airmass.grid(row=2, column=3)
+    obj_finder_plot.grid(row=3, column=3)
+    save_plots.grid(row=4, column=3)
+    close_button.grid(row=6, column=2)
+
+    for i in range(11):
+        top.grid_rowconfigure(i, weight=1)
+    top.grid_columnconfigure(0,weight=1)
+    top.grid_columnconfigure(1,weight=1)
+
+
+    top.mainloop()
+    
+    params_dict= {'obsDate':cal.get_date(),
+    'AltPlot': CheckVarAlt.get(),
+    'AirMassPlot': CheckVarAirmass.get(), 
+    'SaveBothPlots': SavePlots.get(),
+    'ObjSkyPlot':ObjSky.get(),
+    'Object': NameObject.get(), 
+    'RA': RA_Object.get(),
+    'Dec': Dec_Object.get() }
+    
+    return params_dict
+
+if __name__ == "__main__":
+	get_values()
+
